bbe0d27a3c3c58d620ed02f9681b57cf4d81e4d0
qeta422 <qetafetviashvili123@gmail.com>
2024-03-22 19:03:27 +0400

    modal close

--- modal windows/main.js
+++ modal windows/main.js
@@ -8,4 +8,10 @@ for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener("click", function () {
     console.log("Button clicked");
     modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
   });
+
+btnCloseModal.addEventListener("click", function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
