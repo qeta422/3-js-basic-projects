a84230127b291d13e5207d53b7cf24551b77db8a
qeta422 <qetafetviashvili123@gmail.com>
2024-03-22 19:11:28 +0400

    modal refactoring

--- modal windows/main.js
+++ modal windows/main.js
@@ -4,14 +4,19 @@ const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 console.log(btnsOpenModal);
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener("click", function () {
-    console.log("Button clicked");
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-  });
+const openModal = function () {
+  console.log("Button clicked");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
 
-btnCloseModal.addEventListener("click", function () {
+const closedModal = function () {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
-});
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener("click", openModal);
+
+btnCloseModal.addEventListener("click", closedModal);
+overlay.addEventListener("click", closedModal);
