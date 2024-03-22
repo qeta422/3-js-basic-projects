a43833e56190a0feb11a8015a26a2fa29812f69d
qeta422 <qetafetviashvili123@gmail.com>
2024-03-22 19:25:15 +0400

    escape keydown

--- modal windows/main.js
+++ modal windows/main.js
@@ -5,12 +5,11 @@ const btnsOpenModal = document.querySelectorAll(".show-modal");
 console.log(btnsOpenModal);
 
 const openModal = function () {
-  console.log("Button clicked");
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
 };
 
-const closedModal = function () {
+const closeModal = function () {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
 };
@@ -18,5 +17,13 @@ const closedModal = function () {
 for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener("click", openModal);
 
-btnCloseModal.addEventListener("click", closedModal);
-overlay.addEventListener("click", closedModal);
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  console.log(e.key);
+
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
